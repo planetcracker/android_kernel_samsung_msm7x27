6739b9073bed011773ee4d913d60621e382df57e
Andrei Warkentin <andreiw@motorola.com>
2011-04-06 16:33:44 -0500

    MMC: Expose mmc_switch to higher drivers.
    Needed for Sandisk workaround (manipulate EXT_CSD).
    
    Change-Id: I7bfe50a1503ac73ae072db718b60c27526521e41
    Signed-off-by: Andrei Warkentin <andreiw@motorola.com>

--- include/linux/mmc/core.h
+++ include/linux/mmc/core.h
@@ -133,6 +133,7 @@ extern void mmc_wait_for_req(struct mmc_host *, struct mmc_request *);
 extern int mmc_wait_for_cmd(struct mmc_host *, struct mmc_command *, int);
 extern int mmc_wait_for_app_cmd(struct mmc_host *, struct mmc_card *,
 	struct mmc_command *, int);
+extern int mmc_switch(struct mmc_card *, u8, u8, u8);
 
 extern void mmc_set_data_timeout(struct mmc_data *, const struct mmc_card *);
 extern unsigned int mmc_align_data_size(struct mmc_card *, unsigned int);
