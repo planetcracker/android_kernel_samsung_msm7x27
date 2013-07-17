79a43101736c85d6d7ad6da853d9fd7acd8a480f
alin23 <alin.p32@gmail.com>
2013-07-17 18:21:04 +0300

    cpufreq: new Zen governor
    So while searching for a better governor for Galaxy Mini that could beat the
    SmartassV2 by Erasmux, I finally found.. nothing. So I created the Zen
    governor, based on SmartassV2, with some little modifications:
    
    -Dynamic Awake Ideal Frequency that changes based on the idle states of the phone
    -BoostPulse linked to the Synaptics driver
    -User-changeable Boost Frequency parameter (which is 604MHz at the moment)
    -Probably the best balance between battery-life and performance for Mini

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -376,9 +376,9 @@ extern struct cpufreq_governor cpufreq_gov_smartass2;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE)
 extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SAVAGEDZEN)
-extern struct cpufreq_governor cpufreq_gov_savagedzen;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_savagedzen)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ZEN)
+extern struct cpufreq_governor cpufreq_gov_zen;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_zen)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCARY)
 extern struct cpufreq_governor cpufreq_gov_scary;
 #define CPUFREQ_DEFAULT_GOVERNOR    (&cpufreq_gov_scary)
